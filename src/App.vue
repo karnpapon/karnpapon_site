39581278225f4eaa2937531f012ef84bc3436ab3
karnpapon <karnpapon@gmail.com>
2020-03-18 10:25:09 +0700

    add noto-sans-th font

--- src/App.vue
+++ src/App.vue
@@ -89,10 +89,10 @@ export default {
   }
 
   .th-txt {
-    a { font-family: 'Mitr', sans-serif;}
+    a { font-family: 'noto_sans_th', sans-serif; font-style: initial;}
     p { font-family: 'Bai Jamjuree', sans-serif; }
     h1, h2{
-      font-family: 'Mitr', sans-serif;
+      font-family: 'noto_sans_th', sans-serif;
       font-size: 22px;
     }
 
@@ -106,8 +106,12 @@ export default {
     }
 
     .intro{
-      font-family: 'Pridi', sans-serif; 
+      font-family: 'noto_sans_th', sans-serif; 
       font-size: 20px;
+      a{
+        font-family: Arial, Helvetica, sans-serif;
+        text-decoration: underline;
+      }
     }
 
     blockquote{
