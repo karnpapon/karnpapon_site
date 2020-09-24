b0fe1e654500bcdbbc378fa864f904e6f6f1072f
karnpapon <karnpapon@gmail.com>
2020-09-25 02:29:31 +0700

    add travis

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/karnpapon-site/'
+      : '/',
     chainWebpack(config){
       config.module.rule('md')
         .test(/\.md/)
